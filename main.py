f75b46a7a487f40a9280f0667af06446f40474b5
DiVaSkibo <d.v.skibo@student.khai.edu>
2025-10-03 20:09:31 +0200

    Done steepest descent method
    + Steepest Descent method
    + Armijo line search
    * UMO
    * main pyth

--- main.py
+++ main.py
@@ -6,13 +6,16 @@ from src.umo import UMO
 
 def fun(x) -> float:
     return 5. * x[0] * x[0] + 2. * x[1] * x[1] + 4. * x[0] * x[1] + 4. * math.sqrt(5.) * (x[0] + x[1]) - 14.
-def jac(x) -> float:
+def grad(x) -> float:
     return np.array([10. * x[0] + 4. * x[1] + 4. * math.sqrt(5.), 4. * x[0] + 4. * x[1] + 4. * math.sqrt(5.)])
 
 
-# UNCONSTRAINED MULTIVARIABLE OPTIMIZATION
+# БАГАТОВИМІРНА БЕЗУМОВНА ОПТИМІЗАЦІЯ
 
 if __name__ == "__main__":
     print('\n|| UNCONSTRAINED MULTIVARIABLE OPTIMIZATION ||\n')
 
-    model = UMO(fun=fun, x=[.0, .0], jac=jac)
+    model = UMO(fun=fun, x=[.0, .0], grad=grad)
+    model.solve(method='Steepest Descent')
+    print(f'result = {model.result}')
+
--- src/umo.py
+++ src/umo.py
@@ -3,16 +3,48 @@ import numpy as np
 
 
 class UMO:
-    '''Unconstrained Multivariable Optimization'''
+    '''
+    **Unconstrained Multivariable Optimization** - *Багатовимірна Безумовна Оптимізація*
+    '''
 
-    def __init__(self, fun:callable, x:list=[.0, .0], jac:callable=None, eps:float=1e-2):
+    def __init__(self, fun:callable, x:list=[.0, .0], grad:callable=None, eps:float=1e-2, maxiter=1000):
         self.fun = fun
         self.x = np.array(x)
-        self.jac = jac
+        self.grad = grad
         self.eps = eps
+        self.MAXITER = maxiter
         self.result = {'x':x, 'fun':fun(x)}
     
     def solve(self, method:str):
-        match method:
-            case _: raise Exception('! Неправильне введення методу оптимізації !')
+        '''
+        *Оптимізація функції за методом:*
         
+            Метод найшвидшого спуску -> "Steepest Descent"
+        '''
+        match method:
+            case 'Steepest Descent':
+                self.result = self._steepestDescent()
+            case _:
+                raise Exception('! Неправильне введення методу оптимізації !')
+    
+    def _steepestDescent(self) -> dict:
+        '''Метод найшвидшого спуску'''
+        x = self.x
+        eps = self.eps
+        alpha = 0
+        for _ in range(self.MAXITER):
+            if np.linalg.norm(self.grad(x)) < eps: break
+            dx = -self.grad(x)
+            alpha = self._armijo_line_search(x, dx)
+            x += alpha * dx
+        return {'x':x.tolist(), 'fun':self.fun(x).tolist(), 'dx':dx.tolist(), 'alpha':float(alpha)}
+    
+    
+    def _armijo_line_search(self, x:list, dx:list, alpha:float=1., beta:float=0.5, const:float=1e-4):
+        '''Лінійний пошук зі зменшенням кроку за правилом Арміхо'''
+        f_x = self.fun(x)
+        grad_x = self.grad(x)
+        while self.fun(x + alpha * dx) > f_x + const * alpha * np.dot(grad_x, dx):
+            alpha *= beta
+        return alpha
+
