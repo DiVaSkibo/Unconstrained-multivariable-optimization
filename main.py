35f26c95e18fd07936ed45a30471f22f3287057d
DiVaSkibo <d.v.skibo@student.khai.edu>
2025-10-08 21:14:50 +0200

    Done newton`s method
    + Newton`s Method
    * UMO
    * main pyth

--- main.py
+++ main.py
@@ -6,8 +6,10 @@ from src.umo import UMO
 
 def fun(x) -> float:
     return 5. * x[0] * x[0] + 2. * x[1] * x[1] + 4. * x[0] * x[1] + 4. * math.sqrt(5.) * (x[0] + x[1]) - 14.
-def grad(x) -> float:
+def grad(x) -> np.ndarray:
     return np.array([10. * x[0] + 4. * x[1] + 4. * math.sqrt(5.), 4. * x[0] + 4. * x[1] + 4. * math.sqrt(5.)])
+def hesse(x) -> np.ndarray:
+    return np.array([[10., 4.], [4., 4.]])
 
 
 # БАГАТОВИМІРНА БЕЗУМОВНА ОПТИМІЗАЦІЯ
@@ -15,9 +17,11 @@ def grad(x) -> float:
 if __name__ == "__main__":
     print('\n|| UNCONSTRAINED MULTIVARIABLE OPTIMIZATION ||\n')
 
-    model = UMO(fun=fun, x=(.0, .0), grad=grad)
+    model = UMO(fun=fun, x=(.0, .0), grad=grad, hesse=hesse)
     model.solve(method='Steepest Descent')
     print(f'result = {model.result}\n')
     model.solve(method='Conjugate Gradient')
+    print(f'result = {model.result}\n')
+    model.solve(method='Newton')
     print(f'result = {model.result}\n')
 
--- src/umo.py
+++ src/umo.py
@@ -8,11 +8,12 @@ class UMO:
     **Unconstrained Multivariable Optimization** - *Багатовимірна Безумовна Оптимізація*
     '''
 
-    def __init__(self, fun:callable, x:tuple=(.0, .0), grad:callable=None, eps:float=1e-3, maxiter=1000):
+    def __init__(self, fun:callable, x:tuple=(.0, .0), grad:callable=None, hesse:callable=None, eps:float=1e-3, maxiter=1000):
         self.fun = fun
         self.x = np.array(x, dtype=float)
         self.grad = grad
-        self.eps = eps
+        self.hesse = hesse
+        self.EPS = eps
         self.MAXITER = maxiter
         self.result = None
     
@@ -20,8 +21,9 @@ class UMO:
         '''
         Оптимізація функції за методом:
         
-            Метод найшвидшого спуску -> "Steepest Descent"
-            Метод спряжених градієнтів -> "Conjugate Gradient"
+            Метод найшвидшого спуску    ->  "Steepest Descent"
+            Метод спряжених градієнтів  ->  "Conjugate Gradient"
+            Метод Ньютона               ->  "Newton"
         '''
         self.result = None
         match method:
@@ -29,40 +31,56 @@ class UMO:
                 self.result = self._steepestDescent()
             case 'Conjugate Gradient':
                 self.result = self._conjugateGradient()
+            case 'Newton':
+                self.result = self._newton()
             case _:
                 raise Exception('! Неправильне введення методу оптимізації !')
     
     def _steepestDescent(self) -> dict:
         '''Метод найшвидшого спуску'''
         x = self.x.copy()
-        eps = self.eps
+        EPS = self.EPS
         alpha = .0
         i = 0
         for i in range(self.MAXITER):
-            if LA.norm(self.grad(x)) < eps: break
+            if LA.norm(self.grad(x)) < EPS: break
             dx = -self.grad(x)
             alpha = self._armijo_line_search(x, dx)
             x += alpha * dx
-        return {'x':x.tolist(), 'fun':self.fun(x), 'dx':dx.tolist(), 'alpha':float(alpha), 'iter':i}
+        return {'x':x.tolist(), 'fun':float(self.fun(x)), 'grad':dx.tolist(), 'alpha':float(alpha), 'iter':i}
     def _conjugateGradient(self) -> dict:
         '''Метод спряжених градієнтів'''
         x = self.x.copy()
-        eps = self.eps
+        EPS = self.EPS
         alpha = .0
         dx = -self.grad(x)
-        gradient = LA.norm(self.grad(x))
+        grad_norm = LA.norm(self.grad(x))
         i = 0
         for i in range(self.MAXITER):
-            if gradient < eps: break
+            if grad_norm < EPS: break
             alpha = self._armijo_line_search(x, dx)
             x += alpha * dx
-            gradnew = LA.norm(self.grad(x))
-            beta = (gradnew ** 2) / (gradient * gradient)
+            grad_new = LA.norm(self.grad(x))
+            beta = (grad_new ** 2) / (grad_norm * grad_norm)
             dx = dx * beta - self.grad(x)
-            gradient = gradnew
-        return {'x':x.tolist(), 'fun':self.fun(x), 'dx':dx.tolist(), 'alpha':float(alpha), 'grad':float(gradient), 'iter':i}
-    
-    
+            grad_norm = grad_new
+        return {'x':x.tolist(), 'fun':float(self.fun(x)), 'grad':dx.tolist(), 'alpha':float(alpha), 'grad_norm':float(grad_norm), 'iter':i}
+    def _newton(self) -> dict:
+        '''Метод Ньютона'''
+        x = self.x.copy()
+        EPS = self.EPS
+        dx = self.grad(x)
+        deltax = -LA.inv(self.hesse(x)) @ dx
+        grad_norm = LA.norm(dx)
+        i = 0
+        for i in range(self.MAXITER):
+            if grad_norm < EPS: break
+            x += deltax
+            dx = self.grad(x)
+            deltax = -LA.inv(self.hesse(x)) @ dx
+            grad_norm = LA.norm(self.grad(x))
+        return {'x':x.tolist(), 'fun':float(self.fun(x)), 'grad':self.grad(x).tolist(), 'hesse':self.hesse(x).tolist(), 'grad_norm':float(grad_norm), 'iter':i}
+
     def _armijo_line_search(self, x, dx, alpha:float=1., beta:float=0.5, const:float=1e-4):
         '''Лінійний пошук зі зменшенням кроку за правилом Арміхо'''
         f_x = self.fun(x)
